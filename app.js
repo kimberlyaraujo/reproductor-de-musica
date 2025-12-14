68f7f27ada4abf3768deee501f2ec04059492ced
kimberly899 <kimberlymicheller4@gmail.com>
2025-12-14 16:43:35 -0600

    se crearon las constantes de js

---
+++ app.js
@@ -0,0 +1,11 @@
+const titulocancion=document.querySelector('.reproductormusica h1');
+const nombreartista=document.querySelector('.reproductormusica p');
+
+const progreso=document.getElementById('progreso');
+const cancion=document.getElementById('cancion');
+
+const iconocontrol=document.getElementById('iconocontrol');
+const botonreproducir=document.querySelector('.controles button.reproducir');
+
+const botonatras=document.querySelector('.controles button.atras');
+const botonadelante=document.querySelector('.controles button.adelante');
